2fba72a4d32e69a778941e987d1bda3d0f6b1874
Yan Mingzhi <realraindust@gmail.com>
2021-01-20 16:02:52 +0800

    update build in docker script

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,5 +2,5 @@
 
 docker-compose run --rm build-gluon
 
-mkdir -p ../builds/actors
-cp target/wasm32-unknown-unknown/release/gluon_actor_signed.wasm ../builds/actors/
+mkdir -p ../builds/runtime-cache
+cp target/wasm32-unknown-unknown/release/gluon_actor_signed.wasm ../builds/runtime-cache/
